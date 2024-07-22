544eafc2d66fb601d5643d6a91afb325e71665c5
root <root@gen-mtvr-09.mtvr.labs.mlnx>
2024-07-22 04:55:45 +0300

    support .cpp files

--- .vim/mkcscope.sh
+++ .vim/mkcscope.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 find $PWD -name '*.[ch]' -type f -exec echo \"{}\" \; | sort -u > cscope.files
+find $PWD -name '*.cpp' -type f -exec echo \"{}\" \; | sort -u >> cscope.files
 cscope -bvq
